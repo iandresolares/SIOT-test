64ecca1dbca0da45dd1a5f571f97cc8039f28faa
iandresolares <andresolaresjurado@gmail.com>
2022-03-21 15:41:11 +0100

    New version with variable number of devices, more randomization and blueprint dashboards

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ import tago
 import numpy as np
 import datetime
 
-device_token = "f8d2036c-833f-46a5-ba52-bc3d2c8988aa"
-device = tago.Device(device_token)
+
+ACCOUNT_TOKEN = "0acd0e15-bb51-43df-9f5c-968bf0c1292a"
+TAGO_ACCOUNT = tago.Account(ACCOUNT_TOKEN)
 
 
 def reMap(value, maxInput, minInput, maxOutput, minOutput):
@@ -77,62 +78,129 @@ def get_solar_polynomial():
     return poly
 
 
+class TagoDevice:
+    def __init__(self, name: str):
+        self.name = name
+        if not self.find_device(name):
+            self.create_device(name)
+
+    def find_device(self, name: str):
+
+        devices = TAGO_ACCOUNT.devices.list()["result"]
+        for dev in devices:
+            if dev["name"] == name:
+                self.id = dev["id"]
+                self.token = TAGO_ACCOUNT.devices.tokenList(self.id)["result"][-1][
+                    "token"
+                ]
+                self.device = tago.Device(self.token)
+                return True
+        return False
+
+    def create_device(self, name: str):
+        new_device = {
+            "name": name,
+            "description": f"SIOT device.",
+            "active": True,
+            "visible": True,
+            "tags": [{"key": "type", "value": "siot"}],
+        }
+        result = TAGO_ACCOUNT.devices.create(new_device)["result"]
+        self.id = result["device_id"]
+        self.token = result["token"]
+        self.device = tago.Device(self.token)
+
+
 def main():
 
     solar_poly = get_solar_polynomial()
     house_poly = get_house_polynomial()
 
-    # Invent
-    co2 = 37.32
-    euros = 133.43
-    oil = 0.01
-    trees = 17
+    number_of_devices = 10
+    devices_list = list()
+    # * Crear u obtener los dispositivos
+    for n in range(0, number_of_devices):
+        devices_list.append(TagoDevice(name=f"SIOT-DEVICE-{n}"))
+        devices_list[n].co2 = round(random.uniform(5, 60), 2)
+        devices_list[n].euros = round(devices_list[n].co2 * 3.2, 2)
+        devices_list[n].oil = round(devices_list[n].co2 * 0.0029, 2)
+        devices_list[n].trees = int(devices_list[n].co2 * 2.43)
+        devices_list[n].day = None
+        devices_list[n].cont = random.randint(1, 500)
 
-    cont = 0
+        devices_list[n].device.insert({"variable": "co2", "value": devices_list[n].co2})
+        devices_list[n].device.insert(
+            {"variable": "euros", "value": devices_list[n].euros}
+        )
+        devices_list[n].device.insert(
+            {"variable": "trees", "value": devices_list[n].trees}
+        )
+        devices_list[n].device.insert({"variable": "oil", "value": devices_list[n].oil})
 
     while True:
-
         dt = datetime.datetime.now()
         h = dt.hour
         m = dt.minute
         dec_time = (h * 60 + m) / 60
+        for td in devices_list:
+            td.cont += 1
+            if dt.day != td.day:
+                td.day = dt.day
+                td.cont += 1  # * Randomize data
+                td.time_random_param = random.uniform(-0.1, 0.1)
+                if random.randint(1, 3) == 1:
+                    td.house_random_param = random.uniform(0.8, 1.2)
+                else:
+                    td.house_random_param = random.uniform(0.9, 1.1)
+                if random.randint(1, 3) == 1:
+                    td.solar_random_param = random.uniform(0.6, 1.2)
+                else:
+                    td.solar_random_param = random.uniform(0.8, 1.2)
 
-        x_time = random.gauss(dec_time, 0.2)
-        avg_house_consumption = house_poly(x_time)
-        avg_solar_generation = solar_poly(x_time)
+            x_time = random.gauss(dec_time, 0.2 + td.time_random_param)
+            avg_house_consumption = td.house_random_param * house_poly(x_time)
+            avg_solar_generation = td.solar_random_param * solar_poly(x_time)
 
-        house_consumption = round(random.gauss(avg_house_consumption, 0.03), 2)
-        solar_generation = round(random.gauss(avg_solar_generation, 0.03), 2)
+            house_consumption = round(random.gauss(avg_house_consumption, 0.03), 2)
+            solar_generation = round(random.gauss(avg_solar_generation, 0.03), 2)
 
-        grid_consumption = (
-            house_consumption - solar_generation
-        )  # Si negativo se consume de la red, si p
-        if grid_consumption < 0:
-            grid_inyection = abs(grid_consumption)
-            grid_consumption = 0
-        else:
-            grid_inyection = 0
+            grid_consumption = (
+                house_consumption - solar_generation
+            )  # Si negativo se consume de la red, si p
+            if grid_consumption < 0:
+                grid_inyection = abs(grid_consumption)
+                grid_consumption = 0
+            else:
+                grid_inyection = 0
 
-        device.insert({"variable": "house_consumption", "value": house_consumption})
-        device.insert({"variable": "solar_generation", "value": solar_generation})
-        device.insert({"variable": "grid_consumption", "value": grid_consumption})
-        device.insert({"variable": "grid_inyection", "value": grid_inyection})
+            td.device.insert(
+                {"variable": "house_consumption", "value": house_consumption}
+            )
+            td.device.insert(
+                {"variable": "solar_generation", "value": solar_generation}
+            )
+            td.device.insert(
+                {"variable": "grid_consumption", "value": grid_consumption}
+            )
+            td.device.insert({"variable": "grid_inyection", "value": grid_inyection})
 
-        if cont % 100 == 0:
-            co2 += 0.01
-            euros += 0.02
-            device.insert({"variable": "co2", "value": co2})
-            device.insert({"variable": "euros", "value": euros})
-        if cont % 500 == 0:
-            trees += 1
-            device.insert({"variable": "trees", "value": trees})
-        if cont % 10000 == 0:
-            oil += 0.01
-            device.insert({"variable": "oil", "value": oil})
+            if td.cont % 50 == 0:
+                td.co2 += 0.01
+                td.euros += 0.02
+                td.device.insert({"variable": "co2", "value": td.co2})
+                td.device.insert({"variable": "euros", "value": td.euros})
+            if td.cont % 80 == 0:
+                td.trees += 1
+                td.device.insert({"variable": "trees", "value": td.trees})
+            if td.cont % 2000 == 0:
+                td.oil += 0.01
+                td.device.insert({"variable": "oil", "value": td.oil})
 
-        cont += 1
-        print("sent")
-        time.sleep(60)
+            print(f"{td.name} Updated!")
+
+        # ! Cada 5 min
+        print("----------------------------------------------")
+        time.sleep(300)
 
 
 if __name__ == "__main__":
